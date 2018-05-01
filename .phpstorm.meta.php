075b67c1be0935d137d414979ea73ac14a7270c9
VOVANCHO <w-karpov@mail.ru>
2018-05-01 12:42:49 +0300

    dev

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-05-01 08:15:58.
+    * Generated on 2018-05-01 09:08:48.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
